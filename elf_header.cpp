bd8e6e7b8c5e35e8de67d4e2d974434863a65c00
MarioVelasquezN <41805686+MarioVelasquezN@users.noreply.github.com>
2020-02-06 02:16:19 -0600

    Update elf_header.cpp

--- elf_header.cpp
+++ elf_header.cpp
@@ -22,10 +22,21 @@ void MyELF::print_header(){
 
     type[0]=reg_header.E_ident[0];
     type[1]=reg_header.E_ident[1];
-    type[2]='\0';
+    type[2]=reg_header.E_ident[2];
+    type[3]='\0';
 
     cout<<"********************************************"<<endl;
     cout<<"Tipo: "<<reg_header.e_type<<endl;
     cout<<"Maquina: "<<reg_header.e_machine<<endl;
     cout<<"Version: "<<reg_header.e_version<<endl;
+    cout<<"Entry: "<<reg_header.e_entry<<endl;
+    cout<<"Phoff: "<<reg_header.e_phoff<<endl;
+    cout<<"Shoff: "<<reg_header.e_shoff<<endl;
+    cout<<"Flags: "<<reg_header.e_flags<<endl;
+    cout<<"Ehsize: "<<reg_header.e_ehsize<<endl;
+    cout<<"Phentsize: "<<reg_header.e_phentsize<<endl;
+    cout<<"Phnum: "<<reg_header.e_phnum<<endl;
+    cout<<"Shentsize: "<<reg_header.e_shentsize<<endl;
+    cout<<"Shnum: "<<reg_header.e_shnum<<endl;
+    cout<<"Shstrndx: "<<reg_header.e_shstrndx<<endl;
 }
